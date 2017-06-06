575f21c563c6d23395698505414d59274da43675
Eden Liu <eden90267@gmail.com>
2017-06-06 17:44:34 +0800

    Learning ch08

--- ch08/meadowlark/meadowlark.js
+++ ch08/meadowlark/meadowlark.js
@@ -5,6 +5,10 @@ var express = require('express');
 
 var fortune = require('./lib/fortune');
 
+var formidable = require('formidable');
+
+var jqupload = require('jquery-file-upload-middleware');
+
 var app = express();
 
 // 設定handlebar view引擎
@@ -30,6 +34,18 @@ app.use(require('body-parser')());
 app.use(function (req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
     next();
+});
+
+app.use('/upload', function (req, res, next) {
+    var now = Date.now();
+    jqupload.fileHandler({
+        uploadDir: function () {
+            return __dirname + '/public/uploads/' + now;
+        },
+        uploadUrl: function () {
+            return '/uploads/' + now;
+        },
+    })(req, res, next);
 });
 
 // mocked weather data
@@ -106,11 +122,31 @@ app.get('/newsletter', function (req, res) {
     res.render('newsletter', {csrf: 'CSRF token goes here'});
 });
 app.post('/process', function (req, res) {
-    console.log('Form (from querystring): ' + req.query.form);
-    console.log('CSRF token (from hidden form field): ' + req.body._csrf);
-    console.log('Name (from visible from field): ' + req.body.name);
-    console.log('Email (from visible from field): ' + req.body.email);
-    res.redirect(303, '/thank-you');
+    if (req.xhr || req.accepts('json,html') === 'json') {
+        // 如果有錯誤的話，我們會傳送{error:'error description'}
+        res.send({success: true});
+    } else {
+        // 如果有錯誤的話，我們會重新導向一個錯誤網頁
+        res.redirect(303, '/thank-you');
+    }
+});
+app.get('/contest/vacation-photo', function (req, res) {
+    var now = new Date();
+    res.render('contest/vacation-photo', {
+        year: now.getFullYear(),
+        month: now.getMonth()
+    });
+});
+app.post('/contest/vacation-photo/:year/:month', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        if (err) return res.redirect(303, '/error');
+        console.log('received fields:');
+        console.log(fields);
+        console.log('received files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
 });
 
 // 404全部抓取處理程式(中介軟體)
